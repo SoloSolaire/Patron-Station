54b53b69ac62bc8a30eb18a930c301c1f02e1520
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-09-12 19:10:47 -0700

    added code to Query in schemas

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,24 @@
-const { User } = require('../models');
+const { User, Project, Comment } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find();
+        },
+        user: async (parent, { userId }) => {
+            return User.findOne({ id: userId });
+        },
+        
+        projects: async () => {
+            return Project.find();
+        },
+
+        comments: async () => {
+            return Comment.find();
+        },
+    },
+
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
